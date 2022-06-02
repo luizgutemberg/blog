d384eb90356463d9f4ae580c7ede7384040616f3
Luiz Gutemberg Teixeira Barreto <barretoluiz90@gmail.com>
2022-06-02 18:35:39 -0300

    criando routes.js

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import react from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Home from './pages/Home'
+
+const Stack = createNativeStackNavigator ();
+
+function Routes (){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+            name="Home"
+            component={Home}
+        />
+        </Stack.Navigator>
+    )
+}
+
+export default Routes;
