e6ca33e0c0888a16e27f44d11ca5de8ec5aadd5e
Luiz Gutemberg Teixeira Barreto <barretoluiz90@gmail.com>
2022-06-02 15:52:08 -0300

    criando a página  Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import react from "react";
+import {View, Text, StyleSheet } from 'react-native'
+
+export default function Home(){
+    return(
+        <View style={style.container}>
+            <Text>Página Home</Text>           
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
