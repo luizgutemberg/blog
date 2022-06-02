29c786c6fa41de571bd537a847eac3a4d0a4747f
Luiz Gutemberg Teixeira Barreto <barretoluiz90@gmail.com>
2022-06-02 18:00:30 -0300

    executar pagina Home

--- App.js
+++ App.js
@@ -5,9 +5,9 @@ import Home from './src/pages/Home'
 
 export default function App (){
   return(
-    <View>
+    <>
       <StatusBar backgroundColor= "#232630" barStyle="light-content" />
       <Home/>
-    </View>
+    </>
   )
 }
