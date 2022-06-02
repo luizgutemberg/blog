1ea9e7e87100e9bed9209d87b0fcb7437ce1843d
Luiz Gutemberg Teixeira Barreto <barretoluiz90@gmail.com>
2022-06-02 15:37:49 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ export default function App(){
+   return(
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+     <View style={style.container}>
+       <Text style={style.title}>Meu primeiro app</Text>
+
+       <Text style={style.title}>Segundo texto</Text>
+     </View>
+
+   )
+ }
+
+ const style = StyleSheet.create({
+   container:{
+     backgroundColor: '#121212',
+     flex:1,
+     justifyContent: 'center',
+     alignItems: 'center'
+     
+   },
+
+   
+   title:{
+     color:'#FFF',
+     fontSize: 24
+   }
+ })
